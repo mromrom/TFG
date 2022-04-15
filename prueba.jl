37e1fb964c6431b7738dffdb4b85efd10adc7af3
mromrom <moisesromeroromo2@gmail.com>
2022-04-15 12:18:25 +0200

    Se eliminan archivos innecesarios, se aniade funcion aux para boruvka

--- prueba.jl
+++ prueba.jl
@@ -67,37 +67,103 @@ a2=[0 1; 0 0]
 
 b=SimpleDiGraph(a2)
 using SimpleWeightedGraphs
+using SimpleWeightedGraphs
+using Graphs
+sources =      [0,0,1,1,1,2,2,3,3,4,4,4,5,6,7]
+destinations = [1,6,2,7,6,3,4,4,5,5,8,7,8,7,8]
+weights =      [4.0,7.0,9.0,20.0,11.0,6.0,2.0,10.0,5.0,15.0,5.0,1.0,12.0,1.0,3.0]
 
-sources = [0,0,1,1,1,2,2,3,3,4,4,4,5,6,7]
 for i in 1:length(sources)
     sources[i] = sources[i] + 1
 end
 
 
-destinations = [1,6,2,4,6,3,4,4,5,5,7,8,8,7,8]
 for i in 1:length(sources)
     destinations[i] = destinations[i] + 1
 end
-weights = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
 g = SimpleWeightedGraph(sources, destinations, weights)
-add_vertex!(g)
+
+gplot(g)
+
+
 nodelabel = 1:nv(g)
 
 gplot(g, nodelabel=nodelabel)
 x=edges(g)
 gf=collect(edges(g))
-a=gf[1].src
-
+typeof(gf)
+sort(gf, by=src)
+a=gf[1].
+g_h = [1,2]
 connected_components(g)
 
-for i in 1: nv(g)
-    a = collect(edges(g))
-    b = Any[]
-    for e in 1: length(a)
-        if(a[e].src == i)
-            push!(b,a[e]);
+b = Any[]
+function boruvka_algorithm(a_g)
+    x = SimpleWeightedGraph(nv(a_g))
+    a = collect(edges(a_g))
+
+    for i in 1: nv(a_g)
+        b= Any[]
+
+        for e in 1: length(a)
+            c_f=a[e].src
+            d_f=a[e].dst
+            if (c_f == i) | (d_f == i)
+                push!(b,a[e])
+            end
+        end
+        sort!(b, by=weight)
+        l_a=length(b)
+        if l_a != 0
+        add_edge!(x, b[1])
+        ed_a = findfirst(isequal(b[1]), a)
         end
     end
+    return x
 end
+
+j_x = boruvka_algorithm(g)
+edges_b = collect(edges(j_x));
+edges_g = collect(edges(g));
+for i in 1:length(edges_b)
+    ed_x = edges_b[i]
+    ed_a = findfirst(isequal(ed_x),edges_g)
+    popat!(edges_g, ed_a)
+end
+# Funcion aux
+edges_final = Any[]
+for e in 1: length(cm[2])
+    ar=cm[2]
+    p = findall(x-> (x.src == ar[e]) | (x.dst == ar[e]), edges_g)
+    if(p!= nothing)
+        for i in 1:length(p)
+            fuente = edges_g[p[i]].src
+            destino = edges_g[p[i]].dst
+            fuente_f = findfirst(isequal(fuente), ar)
+            destino_f = findfirst(isequal(destino), ar)
+            if (fuente_f == nothing) | (destino_f == nothing)
+                push!(edges_final, edges_g[p[i]])
+            end
+        end
+        keepat!(edges_g, findall(x->findfirst(isequal(x),edges_final)==nothing,edges_g))
+    end
+end
+sort!(edges_final, by=weight)
+add_edge!(j_x,edges_final[1])
+# Funcion aux
+
+nodelabel = 1:nv(j_x)
+gplot(j_x, nodelabel = nodelabel)
+aristas = collect(edges(j_x))
+cm= connected_components(j_x)
+c_m = cm[1]
+c_2 = cm[2]
+function boruvka_aux(initial_edges,component)
+
+
+h=SimpleWeightedGraph(3)
+
+add_edge!(h,1,2)
+gplot(h)
 using Debugger
