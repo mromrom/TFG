be8d45414704837176fa87a91b6441bdb784044c
mromrom <moiromrom@alum.us.es>
2022-04-21 22:30:33 +0200

    Se añade primera versión completa de Hierholzer para grafos dirigidos

--- nuevo.jl
+++ nuevo.jl
@@ -20,25 +20,51 @@ for i in 1:length(sources)
 end
 nodelabel = 1:nv(g)
 a=collect(vertices(g))
+graph_edges = collect(edges(g))
 while(length(collect(edges(g)))!=0)
     out_neighbors(g)
 
 
-curr_path = Any[]
-circuit = Any[]
-function algoritmo(grafo_inicial, vertice_actual)
+
+function hierholzer_algorithm(grafo_inicial)
+    curr_path = Any[]
+    circuit = Any[]
     actual_vertex = 1
     while (length(collect(edges(g)))!=0)
-        out_n = outneighbors(g,vertice_actual)
+        out_n = outneighbors(g,actual_vertex)
         b = length(out_n)
         if (b != 0)
-            rem_edge!(g,vertice_actual,out_n[1])
-            push!(curr_path, vertice_actual)
-            actual_vertex = out_n[1]
+            dst_vertex = out_n[1]
+            if (!isempty(curr_path) && curr_path[end] != actual_vertex)
+                rem_edge!(g,actual_vertex,dst_vertex)
+                push!(curr_path, actual_vertex)
+            elseif (!isempty(curr_path) && curr_path[end] == actual_vertex)
+                rem_edge!(g,actual_vertex,dst_vertex)
+            elseif (isempty(curr_path))
+                rem_edge!(g,actual_vertex,dst_vertex)
+                push!(curr_path, actual_vertex)
+            end
+            actual_vertex = dst_vertex
+        else
+            push!(circuit, actual_vertex)
+            vertex_before = curr_path[end]
+            if (curr_path[end] == actual_vertex)
+                pop!(curr_path)
+                vertex_before = curr_path[end]
+            end
+            actual_vertex = vertex_before
         end
     end
+    push!(curr_path, actual_vertex)
+    reverse!(curr_path)
+    append!(circuit,curr_path)
+    reverse!(circuit)
+    return circuit
 end
-out_neighbors
-cf=outneighbors(g,2)
-algoritmo(g,1)
+g = SimpleDiGraph(5)
+for i in 1:length(sources)
+    add_edge!(g,sources[i], destinations[i])
+end
+res = algoritmo(g)
+!isempty([1,2])
 rem_edge!()
